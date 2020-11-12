9869ad6bea302e657fd037ec01548fb611a06f37
Susik Yoon <susikyoon@gmail.com>
2020-11-12 15:31:18 +0900

    bandwidth

--- src/main/KernelDensityEstimator.java
+++ src/main/KernelDensityEstimator.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class KernelDensityEstimator {
 	private List<KernelCenter> kernelCenters; //kernel centers
 	private int dim; //dimensionality
-	public double[] h;
+	public double[] h; //bandwidth
 	private double cardTotal;
 	
 	
