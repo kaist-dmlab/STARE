ba829a4ba56a9da105431d43c6c638b2c6c5c770
young-eun-nam <68045216+young-eun-nam@users.noreply.github.com>
2021-03-29 07:56:53 +0900

    Update StreamGenerator.java

--- src/main/StreamGenerator.java
+++ src/main/StreamGenerator.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class StreamGenerator {
 	private double[] maxValues; //max values
