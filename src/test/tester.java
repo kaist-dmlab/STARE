0f4e2c1e7f7bd42e59c699e7b55747a848703a08
Youngjun Lee <lteyoungjun@gmail.com>
2021-04-27 09:36:32 +0900

    Update tester.java

--- src/test/tester.java
+++ src/test/tester.java
@@ -1,8 +1,6 @@
 package test;
 import java.io.IOException;
 
-
-
 public class tester {
 	public static void main(String[] args) throws IOException, CloneNotSupportedException {
 		String D = "YahooA1"; //data set
