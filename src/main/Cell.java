ece612a8ee2e39bd725ba8bd0a4302fce119f48b
Youngjun Lee <lteyoungjun@gmail.com>
2021-09-07 13:42:17 +0900

    Update Cell.java

--- src/main/Cell.java
+++ src/main/Cell.java
@@ -3,3 +3,27 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+public class Cell {
+	public ArrayList<Integer> cellIdx; //cell index
+	public int slideID; //slide id
+	public HashMap<ArrayList<Integer>,Cell> childCells; 
+	public HashSet<Tuple> tuples; //tuples
+	public double[] cellCenter; //cell center
+
+	public Cell(ArrayList<Integer> cellIdx,  double[] cellCenter, int slideID){
+		this.cellIdx = cellIdx;
+		this.slideID = slideID;
+		this.tuples = new HashSet<Tuple>();
+		this.cellCenter = cellCenter;
+	}
+
+	public int getCard() {
+		return this.tuples.size();
+	}
+
+	public void addTuple(Tuple t) {
+		this.tuples.add(t);
+	}
+	
+}
+	
